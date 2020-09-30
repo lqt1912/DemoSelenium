8f3a9391b8ccd87daa2d6d70a4126d1b8c372f92
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2020-09-30 22:47:50 +0700

    init

---
+++ DemoSelenium.Tests/AutomatedTest.cs
@@ -0,0 +1,40 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace DemoSelenium.Tests
+{
+    public class AutomatedTest:IDisposable
+    {
+        private readonly IWebDriver _driver;
+        private readonly CustomerRegisterPage _page;
+
+        public AutomatedTest()
+        {
+            _driver = new ChromeDriver();
+            _page = new CustomerRegisterPage(_driver);
+            _page.Navigate();
+
+        }
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void Register()
+        {
+            _page.UsernameInput("addddddd");
+            _page.PasswordInput("aaaaaa");
+            _page.PhoneInput("013242343");
+            _page.DobInput(DateTime.Now);
+            _page.Submit();
+
+            
+
+        }
+    }
+}
---
+++ DemoSelenium.Tests/CustomerRegisterPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoSelenium.Tests
+{
+    public class CustomerRegisterPage
+    {
+        private readonly IWebDriver _driver;
+
+        public CustomerRegisterPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public string Title => _driver.Title;
+        public string Source => _driver.PageSource;
+        private const string URI = "https://localhost:44331/Home/Register";
+
+        private IWebElement UsernameElement => _driver.FindElement(By.Id("username"));
+        private IWebElement PasswordElement => _driver.FindElement(By.Id("password"));
+        private IWebElement DobElement => _driver.FindElement(By.Id("dob"));
+        private IWebElement PhoneElement => _driver.FindElement(By.Id("phone"));
+        private IWebElement Create => _driver.FindElement(By.Id("create"));
+
+        public void Navigate() => _driver.Navigate().GoToUrl(URI);
+
+
+        public void UsernameInput(string username) => UsernameElement.SendKeys(username);
+        public void PasswordInput(string password) => PasswordElement.SendKeys(password);
+        public void DobInput(DateTime dateTime) => DobElement.SendKeys(dateTime.ToString());
+        public void PhoneInput(string phone) => PhoneElement.SendKeys(phone);
+        public void Submit() => Create.Click();
+
+    }
+}
